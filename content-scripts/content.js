3b6dd1d64e678eb3acd8dd0f5abc36db687899eb
Sam Rowe <samrowe@deneb.co.uk>
2023-06-07 21:57:18 +0100

    Fixed other colours

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -4,10 +4,23 @@ chrome.runtime.onMessage.addListener(
             if (request.isChecked) {
                 let allElements = document.querySelectorAll('*');
                 for(let i = 0; i < allElements.length; i++) {
+                     allElements[i].style.fontFamily = 'Arial';
+        }
+            } else {
+                //document.body.style.fontFamily = "";
+                location.reload();
+            }
+        }
+
+        if (request.message === "toggle_font2") {
+            if (request.isChecked) {
+                let allElements = document.querySelectorAll('*');
+                for(let i = 0; i < allElements.length; i++) {
                      allElements[i].style.fontFamily = 'Comic Sans MS';
         }
             } else {
-                document.body.style.fontFamily = "";
+                //document.body.style.fontFamily = "";
+                location.reload();
             }
         }
 
@@ -24,6 +37,44 @@ chrome.runtime.onMessage.addListener(
                 var links = document.getElementsByTagName("link");
                 for (var i = links.length - 1; i >= 0; i--) {
                     if (links[i].href === chrome.runtime.getURL("content-scripts/yellow-overlay.css")) {
+                        links[i].parentNode.removeChild(links[i]);
+                    }
+                }
+            }
+        }
+
+        if (request.message === "toggle_blue_overlay") {
+            if (request.isChecked) {
+                // Load the stylesheet
+                var link = document.createElement("link");
+                link.href = chrome.runtime.getURL("content-scripts/blue-overlay.css");
+                link.type = "text/css";
+                link.rel = "stylesheet";
+                document.getElementsByTagName("head")[0].appendChild(link);
+            } else {
+                // Remove the stylesheet
+                var links = document.getElementsByTagName("link");
+                for (var i = links.length - 1; i >= 0; i--) {
+                    if (links[i].href === chrome.runtime.getURL("content-scripts/blue-overlay.css")) {
+                        links[i].parentNode.removeChild(links[i]);
+                    }
+                }
+            }
+        }
+
+        if (request.message === "toggle_green_overlay") {
+            if (request.isChecked) {
+                // Load the stylesheet
+                var link = document.createElement("link");
+                link.href = chrome.runtime.getURL("content-scripts/green-overlay.css");
+                link.type = "text/css";
+                link.rel = "stylesheet";
+                document.getElementsByTagName("head")[0].appendChild(link);
+            } else {
+                // Remove the stylesheet
+                var links = document.getElementsByTagName("link");
+                for (var i = links.length - 1; i >= 0; i--) {
+                    if (links[i].href === chrome.runtime.getURL("content-scripts/green-overlay.css")) {
                         links[i].parentNode.removeChild(links[i]);
                     }
                 }
--- popup/popup.js
+++ popup/popup.js
@@ -17,6 +17,24 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 document.addEventListener("DOMContentLoaded", function() {
+    const dyslexicFontID = document.getElementById("dyslexicFontID2");
+
+    if (dyslexicFontID){
+        dyslexicFontID.onclick = function() {
+            let isChecked = dyslexicFontID.checked;
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.scripting.executeScript({
+                    target: {tabId: tabs[0].id, allFrames: true},
+                    files: ['content-scripts/content.js']
+                }, function() {
+                    chrome.tabs.sendMessage(tabs[0].id, {message: "toggle_font2", isChecked: isChecked});
+                });
+            });
+        };
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
     const overlay = document.getElementById("yellowOverlay");
 
     if (overlay){
@@ -28,6 +46,42 @@ document.addEventListener("DOMContentLoaded", function() {
                     files: ['content-scripts/content.js']
                 }, function() {
                     chrome.tabs.sendMessage(tabs[0].id, {message: "toggle_yellow_overlay", isChecked: isChecked});
+                });
+            });
+        };
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const overlay = document.getElementById("blueOverlay");
+
+    if (overlay){
+        overlay.onclick = function() {
+            let isChecked = overlay.checked;
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.scripting.executeScript({
+                    target: {tabId: tabs[0].id, allFrames: true},
+                    files: ['content-scripts/content.js']
+                }, function() {
+                    chrome.tabs.sendMessage(tabs[0].id, {message: "toggle_blue_overlay", isChecked: isChecked});
+                });
+            });
+        };
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const overlay = document.getElementById("greenOverlay");
+
+    if (overlay){
+        overlay.onclick = function() {
+            let isChecked = overlay.checked;
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.scripting.executeScript({
+                    target: {tabId: tabs[0].id, allFrames: true},
+                    files: ['content-scripts/content.js']
+                }, function() {
+                    chrome.tabs.sendMessage(tabs[0].id, {message: "toggle_green_overlay", isChecked: isChecked});
                 });
             });
         };
