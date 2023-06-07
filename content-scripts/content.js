1082b149ddce847f5969631eb48db17fd2aab187
Sam Rowe <samrowe@deneb.co.uk>
2023-06-07 17:45:27 +0100

    Works but needs polish

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -1,4 +1,34 @@
-const allElements = document.querySelectorAll('*');
-for(let i = 0; i < allElements.length; i++) {
-    allElements[i].style.fontFamily = 'Comic Sans MS';
-}
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.message === "toggle_font") {
+            if (request.isChecked) {
+                let allElements = document.querySelectorAll('*');
+                for(let i = 0; i < allElements.length; i++) {
+                     allElements[i].style.fontFamily = 'Comic Sans MS';
+        }
+            } else {
+                document.body.style.fontFamily = "";
+            }
+        }
+
+        if (request.message === "toggle_overlay") {
+            if (request.isChecked) {
+                // Load the stylesheet
+                var link = document.createElement("link");
+                link.href = chrome.runtime.getURL("content-scripts/content.css");
+                link.type = "text/css";
+                link.rel = "stylesheet";
+                document.getElementsByTagName("head")[0].appendChild(link);
+            } else {
+                // Remove the stylesheet
+                var links = document.getElementsByTagName("link");
+                for (var i = links.length - 1; i >= 0; i--) {
+                    if (links[i].href === chrome.runtime.getURL("content-scripts/content.css")) {
+                        links[i].parentNode.removeChild(links[i]);
+                    }
+                }
+            }
+        }
+    }
+);
+
--- popup/popup.js
+++ popup/popup.js
@@ -3,10 +3,31 @@ document.addEventListener("DOMContentLoaded", function() {
 
     if (dyslexicFontID){
         dyslexicFontID.onclick = function() {
+            let isChecked = dyslexicFontID.checked;
             chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
                 chrome.scripting.executeScript({
                     target: {tabId: tabs[0].id, allFrames: true},
                     files: ['content-scripts/content.js']
+                }, function() {
+                    chrome.tabs.sendMessage(tabs[0].id, {message: "toggle_font", isChecked: isChecked});
+                });
+            });
+        };
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const overlay = document.getElementById("overlay");
+
+    if (overlay){
+        overlay.onclick = function() {
+            let isChecked = overlay.checked;
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.scripting.executeScript({
+                    target: {tabId: tabs[0].id, allFrames: true},
+                    files: ['content-scripts/content.js']
+                }, function() {
+                    chrome.tabs.sendMessage(tabs[0].id, {message: "toggle_overlay", isChecked: isChecked});
                 });
             });
         };
